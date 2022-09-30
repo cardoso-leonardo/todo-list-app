6356cd568ead006a18c5ef61a95c369981075296
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2022-09-30 14:48:01 -0300

    feat: create XCode project

---
+++ Simple To Do List app/Simple To Do List app/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple To Do List app
+//
+//  Created by Leonardo Cardoso on 30/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
