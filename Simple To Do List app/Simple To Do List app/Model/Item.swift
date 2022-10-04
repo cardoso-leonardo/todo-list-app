58a7c5c93a8072ca6d5f52f2f306a3fc7c478bcf
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2022-10-04 18:28:04 -0300

    feat: install RealmSwift

---
+++ Simple To Do List app/Simple To Do List app/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Simple To Do List app
+//
+//  Created by Leonardo Cardoso on 04/10/22.
+//
+
+import Foundation
+import RealmSwift
+
+class Item: Object {
+    @objc dynamic var title: String = ""
+    @objc dynamic var done: Bool = false
+}
